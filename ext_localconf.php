2de369a994e044f110c3145fc283eaccccbb5e45
Oliver Kroener <ok@oliver-kroener.de>
2024-10-07 17:56:22 +0000

    [TASK] Added config and Service

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,25 +1,3 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-$_EXTKEY = 'ok_cookiebot';
-
-// Register backend module
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-    'OliverKroener.' . $_EXTKEY,
-    'web', // Main module: 'web'
-    'cookieBot', // Submodule key
-    '', // Position
-    [
-        'OliverKroener\\OkCookiebotCookieConsent\\Controller\\ConsentController' => 'index, error, save',
-    ],
-    [
-        'access' => 'user,group',
-        'icon'   => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/module-icon.svg',
-        'labels' => 'LLL:EXT:' . $_EXTKEY . '/Resources/Private/Language/locallang_mod.xlf',
-    ]
-);
-
-// Add TypoScript setup
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-    '@import "EXT:' . $_EXTKEY . '/Configuration/TypoScript/setup.typoscript"'
-);
